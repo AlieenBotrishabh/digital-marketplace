c4c6371d628b298790ed7d510533521e8c4e794d
Rishabh Kumar <119288381+AlieenBotrishabh@users.noreply.github.com>
2025-08-17 17:35:38 +0530

    success

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -28,7 +28,7 @@ async function getData(id: string) {
 }
 
 export default async function ProductPage({ params }: { params: { id: string } }) {
-  const { id } = params; // << Remove 'await'
+  const { id } = params;
   const data = await getData(id);
 
   return (
@@ -51,14 +51,12 @@ export default async function ProductPage({ params }: { params: { id: string } }
         <CarouselPrevious className="ml-16" />
         <CarouselNext className="mr-16" />
       </Carousel>
-
       <div className="lg:col-span-3 lg:flex lg:items-center lg:flex-col">
         <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">{data?.name}</h1>
         <p className="mt-6 text-gray-500">{data?.smallDescription}</p>
         <Button size="lg" className="bg-blue-600 text-white w-full mt-10">
           Buy for ${data?.price}
         </Button>
-
         <div className="w-full border-t border-gray-200 mt-10 pt-10">
           <div className="grid grid-cols-2 w-full gap-y-3">
             <h3 className="text-sm font-medium text-gray-500 col-span-1">Released:</h3>
@@ -69,10 +67,8 @@ export default async function ProductPage({ params }: { params: { id: string } }
             <h3 className="text-sm font-medium col-span-1">{data?.category}</h3>
           </div>
         </div>
-
         <div className="border-t border-gray-200 mt-10"></div>
       </div>
-
       <div className="w-full max-w-2xl mx-auto mt-16 lg:max-w-none lg:mt-0 lg:col-span-4">
         <ProductDescription content={data?.description as JSONContent} />
       </div>
